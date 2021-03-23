11139ca42212d02c51cce26c2ca8dabe445b2abb
Basile Pesin <basile.pesin@vertmo.org>
2021-03-23 22:39:35 +0100

    mot clé in en obc

--- mode-lustre.js
+++ mode-lustre.js
@@ -8,7 +8,7 @@ ace.define("ace/mode/lustre_highlight_rules",["require","exports","module","ace/
         var keywords = "type|node|returns|var|let|tel" +
             "|if|then|else|fby|when|merge|switch|do|end|reset|every|" +
             "automaton|state|until|unless|continue|last|" +
-            "machine|memory|instances|step|case"; // also use obc coloring
+            "machine|memory|instances|step|case|in"; // also use obc coloring
 
         var builtinConstants = (
             "true|false"
